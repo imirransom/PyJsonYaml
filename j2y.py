ac296f03dfb57b08404edf546acc8bbf4a2defab
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-14 16:47:29 -0500

    updated json to yaml converter to take any json as a parameter

--- j2y.py
+++ j2y.py
@@ -6,10 +6,11 @@ def json_to_yaml_converter(json_file):
     with open(json_file, 'r') as file:
         new_file = json.load(file)
 
-    with open("donuts.yaml", "w") as yaml_file:
+    with open(json_file.replace(".json", "") + ".yaml", "w") as yaml_file:
         yaml.dump(new_file, yaml_file)
 
-    with open("donuts.yaml", "r") as yaml_file:
+    with open(json_file, "r") as yaml_file:
         print(yaml_file.read())
+
 
 json_to_yaml_converter("donuts.json")
--- y2j.py
+++ y2j.py
@@ -3,4 +3,8 @@ import json
 
 
 def yaml_to_json_converter(yaml_file):
-    pass
+    with open(yaml_file, "r") as file:
+        new_file = yaml.safe_load(file)
+
+    with open(yaml_file.replace(".yaml", "") + ".json") as file:
+
