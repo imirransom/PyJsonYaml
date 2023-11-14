5ad2b3f25b304c8a5293e11ccc338d5ff9ff903f
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-14 17:15:18 -0500

    done yaml to json converter, can pass any yaml file

--- j2y.py
+++ j2y.py
@@ -6,7 +6,7 @@ def json_to_yaml_converter(json_file):
     with open(json_file, 'r') as file:
         new_file = json.load(file)
 
-    with open(json_file.replace(".json", "") + ".yaml", "w") as yaml_file:
+    with open(json_file.replace(".json", ".yaml"), "w") as yaml_file:
         yaml.dump(new_file, yaml_file)
 
     with open(json_file, "r") as yaml_file:
--- y2j.py
+++ y2j.py
@@ -3,8 +3,9 @@ import json
 
 
 def yaml_to_json_converter(yaml_file):
-    with open(yaml_file, "r") as file:
-        new_file = yaml.safe_load(file)
+    with open(yaml_file, "r") as file, open(yaml_file.replace(".yaml", ".json"), "w") as json_file:
+        yaml_object = yaml.safe_load(file)
+        print(json.dump(yaml_object, json_file))
 
-    with open(yaml_file.replace(".yaml", "") + ".json") as file:
 
+yaml_to_json_converter("verify.yaml")
